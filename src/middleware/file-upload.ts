ad047ceaf15e1c9e16ac5a76b754e0c202f3d3c6
swarup-e <swarup@datakaveri.org>
2025-06-28 17:07:04 +0530

    [Update] - fixed 5Mb limit for asset

--- src/middleware/file-upload.ts
+++ src/middleware/file-upload.ts
@@ -28,8 +28,8 @@ const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
   limits: {
-    fieldSize: 10 * 1024 * 1024, // 10MB file size limit
-    fileSize: 10 * 1024 * 1024, // 10MB file size limit
+    fieldSize: 5 * 1024 * 1024, // 5MB file size limit
+    fileSize: 5 * 1024 * 1024, // 5MB file size limit
   },
   fileFilter: (req, file, cb) => {
     // Check if the file type is allowed
